ba74990def70b925d33e67c85563d13edfac1f10
fatemeh <babaei.f1996@gmail.com>
2024-02-15 23:02:13 +0330

    core: base config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App font-extrabold	text-green-500">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            white: "#ffffff",
+            green: {
+                50: "#effefb",
+                100: "#c7fff3",
+                200: "#90ffe6",
+                300: "#51f7d9",
+                400: "#1de4c6",
+                500: "#04c8ad",
+                600: "#00a693",
+                700: "#058073",
+                800: "#0a655d",
+                900: "#0d544d",
+                950: "#003331",
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
+};
