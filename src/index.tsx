edffa50f447e61511542b707a6d659afc225c1a5
fatemeh <babaei.f1996@gmail.com>
2024-02-16 23:10:41 +0330

    feat: add font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import './assets/css/app.css';
+import './assets/css/font.css'
 import { BrowserRouter } from 'react-router-dom';
 import AppRoute from './AppRoute';
 const root = ReactDOM.createRoot(
