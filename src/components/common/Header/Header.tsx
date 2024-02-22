5afca1a31c0f260dbc3dec1e9e71010c34d4ebe6
fatemeh <babaei.f1996@gmail.com>
2024-02-22 14:36:03 +0330

    fix:fix title page with use path name

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className="header-box p-[22px]">کیف پول</header>
-  )
-}
+  const location = useLocation();
+  const getTitle = (pathname: string) => {
+    switch (pathname) {
+      case '/':
+        return 'لیست داشبورد';
+      case '/services':
+        return 'لیست خدمات';
+      case '/requests':
+        return 'درخواست خدمات';
+      case '/discount':
+        return 'کیف پول';
+      case '/profile':
+        return 'پروفایل';
+      case '/logout':
+        return 'خروج';
+      default:
+        return '404 page';
+    }
+  };
 
-export default Header
+  return (
+    <header className="header-box p-[22px]">{getTitle(location.pathname)}</header>
+  );
+};
+
+export default Header;
