a6d1d883ba9333bdba2604485e2e7744d7310c04
fatemeh <babaei.f1996@gmail.com>
2024-02-22 13:37:18 +0330

    chore: without validation

--- src/components/CalcDiscount/CalcDiscount.jsx
+++ src/components/CalcDiscount/CalcDiscount.jsx
@@ -1,14 +1,34 @@
 import { getDisCount } from "src/core/utils/getDiscount";
 import Button from "../common/Button/Button";
-import { useAsyncError } from "react-router-dom";
 import { useState } from "react";
+
 const CalcDiscount = () => {
-    const getDiscountResult = getDisCount(20, 800);
-    const [disCountVal, setDiscountVal] = useState(0);
-    const [num, setNum] = useState(0);
+    const [formData, setFormData] = useState({
+        discount: 0,
+        num: 0,
+    });
+    const [discountResult, setDiscountResult] = useState(null);
+
+    const generateDiscountHandler = (e) => {
+        e.preventDefault();
+        const result = getDisCount(formData.discount, formData.num);
+        setDiscountResult(result);
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
     return (
         <>
-            <form className="flex flex-col md:flex-row md:gap-[27px] items-end flex-wrap">
+            <form
+                className="flex flex-col md:flex-row md:gap-[27px] items-end flex-wrap"
+                onSubmit={generateDiscountHandler}
+            >
                 <div className="max-w-[406px] w-full flex flex-col">
                     <label
                         htmlFor="discount"
@@ -17,6 +37,7 @@ const CalcDiscount = () => {
                         درصد
                     </label>
                     <input
+                        onChange={handleInputChange}
                         className="sp-border rounded-sm py-[15px] px-[24px] h-[66px] bg-gray-700 font-normal text-base text-white placeholder:text-white placeholder:text-base"
                         type="number"
                         name="discount"
@@ -32,6 +53,7 @@ const CalcDiscount = () => {
                         عدد مورد نظر را وارد کنید
                     </label>
                     <input
+                        onChange={handleInputChange}
                         className="sp-border rounded-sm py-[15px] px-[24px] h-[66px] max-w-[406px] bg-gray-700 font-normal text-base text-white placeholder:text-white placeholder:text-base "
                         name="num"
                         type="text"
@@ -39,16 +61,21 @@ const CalcDiscount = () => {
                         placeholder="مقدار عدد خود را وارد کنید"
                     />
                 </div>
-                <Button className="h-[66px] px-[28px] py-[15px] rounded-sm font-extrabold bg-green-500 max-w-[178px]">
+                <Button
+                    type="submit"
+                    className="h-[66px] px-[28px] py-[15px] rounded-sm font-extrabold bg-green-500 max-w-[178px]"
+                >
                     %محاسبه
                 </Button>
             </form>
-            <div className="flex justify-between items-center bg-gray-700 text-white px-[24px] py-[44px] my-8">
-                <span>
-                    {disCountVal}٪ عدد {num} برابر است:
-                </span>
-                <span>{getDiscountResult}</span>
-            </div>
+            {discountResult !== null && (
+                <div className="flex justify-between items-center bg-gray-700 text-white px-[24px] py-[44px] my-8">
+                    <span>
+                        {formData.discount}٪ عدد {formData.num} برابر است:
+                    </span>
+                    <span>{discountResult}</span>
+                </div>
+            )}
         </>
     );
 };
